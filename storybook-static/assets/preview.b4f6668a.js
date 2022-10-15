25eeb9fff347c5c487551b8bb88256cce768cc7a
Samuel1s <samuelfds286@gmail.com>
2022-10-14 21:55:48 -0300

    diretory changed to root repository

---
+++ storybook-static/assets/preview.b4f6668a.js
@@ -0,0 +1,2 @@
+import{t as o}from"./index.3e65099d.js";import"./iframe.38573000.js";import"./es.map.constructor.390f8c67.js";import"./es.number.to-fixed.826cbe19.js";import"./index.1295e713.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/},docs:{theme:o.dark}}};export{s as parameters};
+//# sourceMappingURL=preview.b4f6668a.js.map
